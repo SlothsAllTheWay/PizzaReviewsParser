399e4ff1a25901427847997aa0d66bab6843add5
SlothsAllTheWay <tacozombie13@yahoo.com>
2021-10-05 18:58:39 -0400

    made getting full name of restaurant faster

--- main.py
+++ main.py
@@ -14,22 +14,29 @@ def getFullName(row: BeautifulSoup)->str:
 
     return name
 
-def getData(row: BeautifulSoup)->Tuple:
+def getData(row: BeautifulSoup)->Tuple[str]:
     score=row.find(class_="jsx-845469894 rating__score").text
     name=row.find(class_="jsx-574827726 reviewCard__title").text
     city_and_state=row.find(class_="jsx-574827726 reviewCard__location").text
 
     city, state=city_and_state.split(", ")
 
-    if "..." in name:
-        name=getFullName(row)
-
     """try:
         score=float(score)
     except ValueError:
         score=0"""
 
-    return (score, name, city, state)
+    return [score, name, city, state]
+
+async def getFullNames(urls: dict[BeautifulSoup, str])->dict[BeautifulSoup, str]:
+    async with httpx.AsyncClient() as client:
+        tasks = (client.get(url, timeout=None) for url in urls.values())
+        reqs = await asyncio.gather(*tasks)
+
+    htmls = [BeautifulSoup(req.text, "html.parser") for req in reqs]
+    names={item: html.find({"h1":"jsx-84601126"}).text for item, html in zip(urls.keys(), htmls)}
+
+    return names
 
 async def getHTMLs(urls: list[str])->list[Tuple]:
     async with httpx.AsyncClient() as client:
@@ -45,12 +52,22 @@ async def getHTMLs(urls: list[str])->list[Tuple]:
 
     data=[getData(row) for row in rows]
     
-    return data
+    return data, rows
 
 def main():
     urls=[BASE_URL.format(i) for i in range(1, NUM+1)]
 
-    data=asyncio.run(getHTMLs(urls))
+    data, rows=asyncio.run(getHTMLs(urls))
+
+    urls={id(item): "https://onebite.app"+row.find(class_="jsx-574827726")["href"] for item, row in zip(data, rows) if "..." in item[1]}
+        
+    names=asyncio.run(getFullNames(urls))
+
+    for item in data:
+        try:
+            item[1]=names[id(item)]
+        except KeyError:
+            pass
 
     with open("data.csv", "w", encoding="UTF-8") as f:
         f.write("score\tname\tcity\tstate\n")
